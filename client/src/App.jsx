ae92fe79ea68d0e5181b06f3751faaa5d0e35bbf
Sanskar Mahato <sanskarmahato@gmail.com>
2024-03-29 12:23:12 +0530

    Created pages and routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import AboutUs from './pages/AboutUs';
+import Events from './pages/Events';
+import ContactUs from './pages/ContactUs';
+
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-up" element={<Signup/>}/>
+    <Route path="/about" element={<AboutUs/>}/>
+    <Route path="/events" element={<Events/>}/>
+    <Route path="/contact" element={<ContactUs/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
