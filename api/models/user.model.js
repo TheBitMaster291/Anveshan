4132f3b6d859c9ed5c6b2f3a6acffa3a6861e0f4
Sanskar Mahato <sanskarmahato@gmail.com>
2024-04-01 15:23:50 +0530

    Created user model.

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+//Different rules for user data in the database
+
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+
+},{timestamps:true}); //Adds the time of entry
+
+const User=mongoose.model('User',userSchema);
+
+export default User;
